2c64b12225e864dbe7a744f6c472c41de1baccd2
SlickFromMars <minecraftsummer2017@gmail.com>
2022-05-03 09:39:01 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Shredded-Plugin
+ 
---
+++ __init__.py
@@ -0,0 +1,10 @@
+bl_info = {
+    'name':     'Shredded Blender Plugin',
+    'blender':  (3, 0, 0),
+    'version':  (0, 0, 1),
+    'author':   'SlickFromMars',
+    "category": "Object"
+}
+
+if __name__ == '__main__':
+    register()
