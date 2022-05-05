cf5e962bbe37ab04b31ca6b10f01d45b3a39bcd4
SlickFromMars <minecraftsummer2017@gmail.com>
2022-05-05 14:51:13 -0400

    cool sidebar work

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # Shredded - A Blender Plugin
 
 Many useful tools for modeling will be added.
+
+Current Features:
+* 1 Preset Mesh
+
+Planned Features:
+* More Preset Meshes
+* Auto Rigging
+* Cursor Center Button
+* Landscape Gem
+* Custom Icons
+* Galaxy Volume
  
--- plugin/__init__.py
+++ plugin/__init__.py
@@ -1,9 +1,9 @@
 bl_info = {
     "name": "Shredded Blender Plugin",
     "author": "SlickFromMars",
-    "version": (0, 1),
+    "version": (0, 2),
     "blender": (2, 80, 0),
-    "location": "View 3D > Sidebar > Shredded",
+    "location": "View 3D > Sidebar > Create Tab",
     "description": "A powerful plugin with many functions.",
     "warning": "",
     "doc_url": "",
@@ -13,15 +13,20 @@ bl_info = {
 if "bpy" in locals():
     import importlib
     importlib.reload(add_mesh_sus)
+    importlib.reload(sidebar_setup)
 else:
     from . import add_mesh_sus
+    from . import sidebar_setup
 
 import bpy
 
 
 def register():
     add_mesh_sus.register()
+    sidebar_setup.register()
+    
 
 def unregister():
     add_mesh_sus.unregister()
+    sidebar_setup.unregister()
     
