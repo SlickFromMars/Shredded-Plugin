31cb3835731dd3ee45bc4f5f1d35d222e8763701
SlickFromMars <minecraftsummer2017@gmail.com>
2022-05-10 14:51:19 -0400

    fixing some goofy errors

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Shredded Blender Plugin",
     "author": "SlickFromMars",
-    "version": (0, 2, 6),
+    "version": (0, 3, 0),
     "blender": (2, 80, 0),
     "location": "View 3D > Sidebar > Create Tab",
     "description": "A powerful plugin with many functions.",
@@ -14,11 +14,9 @@ if "bpy" in locals():
     import importlib
     importlib.reload(shaders)
     importlib.reload(meshes)
-    importlib.reload(icons)
 else:
     from . import shaders
     from . import meshes
-    from . import icons
 
 
 import bpy
@@ -32,26 +30,24 @@ classes = [
 def register():
     print("Loading SHREDDED...")
 
+    shaders.register()
+    meshes.register()
+
     from bpy.utils import register_class
     for cls in classes:
         register_class(cls)
-
-    shaders.register()
-    meshes.register()
-    icons.register_icons()
 
     print("\nLoaded successfully.")
     
 def unregister():
     print("Unloading SHREDDED...")
 
+    shaders.unregister()
+    meshes.unregister()
+
     from bpy.utils import unregister_class
     for cls in reversed(classes):
         unregister_class(cls)
-
-    shaders.unregister()
-    meshes.unregister()
-    icons.unregister_icons()
 
     print("\nUnloaded successfully. Sorry to see you go.")
     
